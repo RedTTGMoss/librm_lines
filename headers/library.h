3a15dda186d8abfb205befbcfb07682a9a818b01
JustRedTTG <redtonehair@gmail.com>
2025-04-06 18:45:51 +0300

    Add debug logger, reformat code

--- headers/library.h
+++ headers/library.h
@@ -10,24 +10,22 @@
 
 #define PI 3.14159265358979323846
 
-// Logging function pointer type
-typedef void (*LogFunc)(const char*);
+typedef void (*LogFunc)(const char*); // Function pointer type for logging functions
 
-// Global log function (can be set externally)
+// Logger
 static LogFunc globalLogger = nullptr;
-
 EXPORT void setLogger(LogFunc logger);
-
 void logMessage(const std::string& msg);
 
-// Logging function pointer type
-typedef void (*ErrorFunc)(const char*);
 
-// Global log function (can be set externally)
-static ErrorFunc globalErrorLogger = nullptr;
-
-EXPORT void setErrorLogger(ErrorFunc errorLogger);
-
+// Error logger
+static LogFunc globalErrorLogger = nullptr;
+EXPORT void setErrorLogger(LogFunc errorLogger);
 void logError(const std::string& msg);
+
+// Debug logger
+static LogFunc globalDebugLogger = nullptr;
+EXPORT void setDebugLogger(LogFunc debugLogger);
+void logDebug(const std::string& msg);
 
 #endif //LIBRARY_H
--- src/library.cpp
+++ src/library.cpp
@@ -1,23 +1,34 @@
 #include "library.h"
 #include <iostream>
 
+// Logger
 EXPORT void setLogger(const LogFunc logger) {
     globalLogger = logger;
 }
-
 void logMessage(const std::string& msg) {
     if (globalLogger) {
         globalLogger(msg.c_str());
     }
 }
+
+// Error logger
 EXPORT void setErrorLogger(const LogFunc errorLogger) {
     globalErrorLogger = errorLogger;
 }
-
 void logError(const std::string& msg) {
     if (globalErrorLogger) {
         globalErrorLogger(msg.c_str());
     } else {
         std::cerr << msg << std::endl;
+    }
+}
+
+// Debug logger
+EXPORT void setDebugLogger(const LogFunc debugLogger) {
+    globalDebugLogger = debugLogger;
+}
+void logDebug(const std::string& msg) {
+    if (globalDebugLogger) {
+        globalDebugLogger(msg.c_str());
     }
 }
