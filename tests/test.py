c82315cf7a7c4d70f6f9e6d9ef04c49bfd9c6170
RedTTG <red@redttg.com>
2025-04-09 20:27:38 +0300

    Improve test

--- tests/test.py
+++ tests/test.py
@@ -5,8 +5,6 @@ import os
 import time
 from colorama import Fore
 
-os.makedirs('output', exist_ok=True)
-
 colorama.init()
 
 
@@ -19,15 +17,21 @@ def python_logger(msg):
 def python_error_logger(msg):
     print(f"{Fore.RED}{msg.decode()}{Fore.RESET}")
 
+
 @ctypes.CFUNCTYPE(None, ctypes.c_char_p)
 def python_debug_logger(msg):
     print(f"{Fore.LIGHTYELLOW_EX}{msg.decode()}{Fore.RESET}")
 
+
 script_folder = os.path.dirname(os.path.abspath(__file__))
+output_folder = os.path.join(script_folder, 'output')
+files_folder = os.path.join(script_folder, 'files')
+
+os.makedirs(output_folder, exist_ok=True)
 
 if os.name == 'nt':
     # Windows-specific code
-    lib = ctypes.WinDLL(os.path.join(script_folder, 'build', 'Debug',  'rm_lines.dll'))
+    lib = ctypes.WinDLL(os.path.join(script_folder, 'build', 'Debug', 'rm_lines.dll'))
 else:
     # Unix-specific code (Linux, macOS)
     lib = ctypes.CDLL(os.path.join(script_folder, '..', 'cmake-build-debug', 'librm_lines.so'))
@@ -41,12 +45,12 @@ lib.setErrorLogger(python_error_logger)
 lib.setDebugLogger(python_debug_logger)
 
 begin_all = time.time()
-for file in os.listdir("files"):
-    output_path = os.path.join('output', file.replace('.rm', '.svg'))
+for file in os.listdir(files_folder):
+    output_path = os.path.join(output_folder, file.replace('.rm', '.svg'))
     with open(output_path, 'w') as f:
         pass
     print("Processing file:", file)
-    with open(os.path.join("files", file), "r+b") as fin, open(output_path, "r+b") as fout:
+    with open(os.path.join(files_folder, file), "r+b") as fin, open(output_path, "r+b") as fout:
         # Call the shared library (it will expand the output file)
         begin = time.time()
         success = lib.convertToSvg(fin.fileno(), fout.fileno())
