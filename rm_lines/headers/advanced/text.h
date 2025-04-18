3c4f4b5e7af46d923b05741ae4c1e25123f6181b
RedTTG <red@redttg.com>
2025-04-18 04:05:56 +0300

    Move special anchors to text.h

--- rm_lines/headers/advanced/text.h
+++ rm_lines/headers/advanced/text.h
@@ -4,6 +4,22 @@
 #include "common/data_types.h"
 #include "common/crdt_sequence_item.h"
 
+constexpr std::array<std::pair<ParagraphStyle, int>, 8> LineHeights = {
+    {
+        {BASIC, 100},
+        {PLAIN, 71},
+        {HEADING, 150},
+        {BOLD, 70},
+        {BULLET, 35},
+        {BULLET2, 35},
+        {CHECKBOX, 100},
+        {CHECKBOX_CHECKED, 100}
+    }
+};
+
+constexpr CrdtId ANCHOR_ID_START = {0, 281474976710654};
+constexpr CrdtId ANCHOR_ID_END = {0, 281474976710655};
+
 struct TextSequence : CrdtSequence<TextItem> {
     void expandTextItems();
 
--- rm_lines/headers/renderer/renderer.h
+++ rm_lines/headers/renderer/renderer.h
@@ -6,23 +6,14 @@
 
 #define TEXT_TOP_Y -88
 
-constexpr std::array<std::pair<ParagraphStyle, int>, 8> lineHeights = {{
-    {BASIC, 100},
-    {PLAIN, 71},
-    {HEADING, 150},
-    {BOLD, 70},
-    {BULLET, 35},
-    {BULLET2, 35},
-    {CHECKBOX, 100},
-    {CHECKBOX_CHECKED, 100}
-}};
-
 class Renderer {
 public:
     explicit Renderer(SceneTree *sceneTree);
+
     ~Renderer();
 
     void calculateAnchors();
+
 private:
     SceneTree *sceneTree;
     std::unordered_map<CrdtId, float> anchors;
