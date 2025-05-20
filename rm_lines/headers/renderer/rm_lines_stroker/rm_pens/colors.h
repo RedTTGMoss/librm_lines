e00b964aece3f25cb3be50a1d188693af326216c
RedTTG <red@redttg.com>
2025-05-20 10:48:44 +0300

    Use braced-init for struct Color

--- rm_lines/headers/renderer/rm_lines_stroker/rm_pens/colors.h
+++ rm_lines/headers/renderer/rm_lines_stroker/rm_pens/colors.h
@@ -3,19 +3,19 @@
 #include "common/data_types.h"
 
 constexpr std::pair<PenColor, Color> rMPallet[] = {
-    {BLACK, Color(0, 0, 0, 255)},
-    {GRAY, Color(125, 125, 125, 255)},
-    {WHITE, Color(255, 255, 255, 255)},
-    {YELLOW, Color(255, 255, 99, 255)},
-    {GREEN, Color(0, 255, 0, 255)},
-    {PINK, Color(255, 20, 147, 255)},
-    {BLUE, Color(0, 98, 204, 255)},
-    {RED, Color(217, 7, 7, 255)},
-    {GRAY_OVERLAP, Color(125, 125, 125, 255)},
-    {GREEN_2, Color(145, 218, 113, 255)},
-    {CYAN, Color(116, 210, 232, 255)},
-    {MAGENTA, Color(192, 127, 210, 255)},
-    {YELLOW_2, Color(250, 231, 25, 255)}
+    {BLACK, Color{0, 0, 0, 255}},
+    {GRAY, Color{125, 125, 125, 255}},
+    {WHITE, Color{255, 255, 255, 255}},
+    {YELLOW, Color{255, 255, 99, 255}},
+    {GREEN, Color{0, 255, 0, 255}},
+    {PINK, Color{255, 20, 147, 255}},
+    {BLUE, Color{0, 98, 204, 255}},
+    {RED, Color{217, 7, 7, 255}},
+    {GRAY_OVERLAP, Color{125, 125, 125, 255}},
+    {GREEN_2, Color{145, 218, 113, 255}},
+    {CYAN, Color{116, 210, 232, 255}},
+    {MAGENTA, Color{192, 127, 210, 255}},
+    {YELLOW_2, Color{250, 231, 25, 255}}
 };
 
 inline Color blendMultiply(const Color base, const Color blend, const float blend_amount) {
